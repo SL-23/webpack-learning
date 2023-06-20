f4242ee34bdb2fc2900340b7fb2c0a9a7f3c3c53
Stephanie Liu <stephanie.liu@rea-group.com>
2023-06-20 23:30:21 +0930

    CleanWebpackPlugin

--- webpack/webpack.common.config.js
+++ webpack/webpack.common.config.js
@@ -1,15 +1,16 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const config = {
   // default get index.js
   // absolute path!!!!
   entry: "./src/js",
   output: {
     path: path.resolve(__dirname, "../dist"),
-    clean: {
-      // dry: true, // just tell you what will be done, not doing anything
-      keep: /\.css/,
-    },
+    // clean: {
+    //   // dry: true, // just tell you what will be done, not doing anything
+    //   keep: /\.css/,
+    // }, --> no need as clean webpack plug in doing same thing
   },
   module: {
     rules: [
@@ -28,6 +29,7 @@ const config = {
       filename: "index.html",
       template: "src/template.html",
     }),
+    new CleanWebpackPlugin(),
   ],
 };
 
